fd7e3930a4391ce37ff3a26dabea716a2f058658
Akash Chouhan <akash.c1500@gmail.com>
2021-10-26 22:08:40 +0530

    disjoint_set class init

--- disjointset.hpp
+++ disjointset.hpp
@@ -2,22 +2,35 @@
 #define disjointset_h
 #include <vector>
 
-template <class T>
-class disjointset
+/* Note to self
+rank init to 1 [Default]
+rank -> set's max depth.
+*/
+
+template <class type>
+class disjoint_set
 {
 public:
-    disjointset(__int32 n = 0) : _groupCount(n) { createDisjointSet(); }
+    disjoint_set(__int32 n = 0) : _groupCount(n), _setcount(std::vector(_groupCount, 1)) { createDisjointSet(); }
+    void union_set(type, type);
+    type find_parent(type);
+    inline bool is_valid(type &) const;
+    bool detect_cycle(type &, type &);
 
 private:
     __int32 _groupCount;
-    std::vector<T> _parent;
-    std::vector<T> _rank;
+    std::vector<type> _parent;
+    std::vector<type> _rank;
+    std::vector<type> _setcount;
     inline void createDisjointSet();
 };
 
-void disjointset::createDisjointSet()
+template <class type>
+void disjoint_set<type>::createDisjointSet()
 {
-    // create a group.
+    _parent = std::vector<type>(_groupCount);
+    _rank = std::vector<type>(_groupCount, 1);
+    for (type i = 0; i < _groupCount; i++)
+        _parent[i] = i;
 }
-
 #endif
