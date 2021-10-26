5b235ef072fcbb4ab309af971c32295b0b4ab8bb
Akash Chouhan <akash.c1500@gmail.com>
2021-10-26 22:21:00 +0530

    added union_set method

--- disjointset.hpp
+++ disjointset.hpp
@@ -40,14 +40,7 @@ bool disjoint_set<type>::detect_cycle(type &node_id1, type &node_id2)
     return (find_parent(node_id1) == find_parent(node_id2));
 }
 
-/*  
- 1 2 3 4 5 -> 5
- Union(12) [12, 3,4,5] -> 4
- union(34) [12, 34, 5] -> 3
- Union(12,34) [1234, 5] -> 2
-
-*/
-/*Implementation of find_parent using Path Compression*/
+/* Implementation of find_parent using Path Compression */
 template <class type>
 type disjoint_set<type>::find_parent(type node_id)
 {
@@ -58,4 +51,48 @@ type disjoint_set<type>::find_parent(type node_id)
     return _parent[node_id] = find_parent(_parent[node_id]);
 }
 
+/*To merge two groups with node ids -> node_id1 and node_id2 */
+template <class type>
+void disjoint_set<type>::union_set(type node_id1, type node_id2)
+{
+    // need isValid
+    type group_id1 = find_parent(node_id1);
+    type group_id2 = find_parent(node_id2);
+    bool flag = false;
+    if (not detect_cycle(node_id1, node_id2))
+    {
+        _groupCount -= 1;
+        flag = true;
+        if (_rank[group_id1] < _rank[group_id2])
+        {
+            _parent[group_id1] = group_id2;
+            _setcount[group_id2] += _setcount[group_id1];
+        }
+        else if (_rank[group_id1] > _rank[group_id2])
+        {
+            _parent[group_id2] = group_id1;
+            _setcount[group_id1] += _setcount[group_id2];
+        }
+        else
+        {
+            _parent[group_id2] = group_id1;
+            _setcount[group_id1] += _setcount[group_id2];
+            _rank[group_id1] += 1;
+        }
+    }
+
+    if (not flag)
+    {
+        std::cerr << "[*]Invalid Operation -> cycle detected(" << node_id1 << "," << node_id2 << ")";
+        std::cout << std::endl;
+    }
+    return;
+}
 #endif
+
+/* Temp dry run.
+ 1 2 3 4 5 -> 5
+ Union(12) [12, 3,4,5] -> 4
+ union(34) [12, 34, 5] -> 3
+ Union(12,34) [1234, 5] -> 2
+*/
