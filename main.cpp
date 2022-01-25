5fbea255381ef6e348ee5fd03d280760043ca770
Akash Chouhan <akash.c1500@gmail.com>
2022-01-25 21:48:16 +0530

    initialise makefile

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "disjoint_set.hpp"
+using namespace std;
+
+/*
+You can utilse this template to execute your codes that requires access to disjoint_set union class.
+* Make the changes to main.cpp and save it.
+* On your terminal:
+    1] make : To compile the the code and header into single output executible.
+    2]./output : To run the executable file.
+*/
+int main()
+{
+
+    long long N;
+    cin >> N;
+    disjoint_set<long long> dsu(N);
+    // Code goes here
+
+    return 0;
+}
